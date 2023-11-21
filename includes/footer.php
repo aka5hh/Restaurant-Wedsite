adbcb511ddb57fdf2f46212733b25caa10800fe0
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-11-21 15:31:13 +0600

    working on icons

--- includes/footer.php
+++ includes/footer.php
@@ -83,7 +83,8 @@
   <script src="js/owl.carousel.min.js"></script>
   <script src="js/isotope.pkgd.min.js"></script>
   <script src="js/jquery.nice-select.min.js"></script>  
-  <script src="js/custom.js"></script>  
+  <script src="js/custom.js"></script>
+  <script src="js/all.min.js"></script>  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap"></script>
 
